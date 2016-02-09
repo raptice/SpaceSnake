45e475fb29fa0cef6cc9b90c6827025447b35ae2
SamtherealMVP <victor.christoffersson@telia.com>
2016-02-09 23:49:35 +0100

    Added the lost posVector code
    Something got lost, I added it again.

--- src/Model/posVector.java
+++ src/Model/posVector.java
@@ -1,42 +1,40 @@
 package Model;
-import java.util.*;
-
 public class posVector {
-	private float xPosition, yPosition;		//Objektets position
-	private float xSpeed, ySpeed;			//Hastighet och riktning
+	private double xPosition, yPosition;		//Objektets position
+	private double xSpeed, ySpeed;			//Hastighet och riktning
 	
-public posVector(float speed, float angleDegree, float xPosition, float yPosition){
-		this.xSpeed = (float)(speed * Math.cos(Math.toRadians(angleDegree)));
-		this.ySpeed = (float)(-speed * Math.sin(Math.toRadians(angleDegree)));
+public posVector(double speed, double angleDegree, double xPosition, double yPosition){
+		this.xSpeed = speed * Math.cos(Math.toRadians(angleDegree));
+		this.ySpeed = -speed * Math.sin(Math.toRadians(angleDegree));
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
 }
-/*public posVector(float xSpeed, float ySpeed, float xPosition, float yPosition){
+/*public posVector(double xSpeed, double ySpeed, double xPosition, double yPosition){
 		this.xSpeed = xSpeed;
 		this.ySpeed = ySpeed;
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
 }*/
 //Hämta vektorns längd ( magnitud )
-public float getSpeedMagnitude(){
-	return (int)Math.sqrt(xSpeed*xSpeed + ySpeed*ySpeed);
+public double getSpeedMagnitude(){
+	return Math.sqrt(xSpeed*xSpeed + ySpeed*ySpeed);
 }
 //Hämta vilken mellan x-planet och vekorn
-public float getAngleDegree(){
-	return (float)Math.toDegrees(Math.atan2(-ySpeed, xSpeed));
+public double getAngleDegree(){
+	return Math.toDegrees(Math.atan2(-ySpeed, xSpeed));
 }
-public float scalar(posVector vector1){
-	return (this.xSpeed*vector1.getX() + this.ySpeed*vector1.getY());
+public double scalar(posVector vector1){
+	return (this.xSpeed*vector1.xSpeed + this.ySpeed*vector1.ySpeed);
 }
 //addera given inparameters vektor med denna vektorn
 public void addVector(posVector vector1){
-	this.xSpeed += vector1.getX();
-	this.ySpeed += vector1.getY();
+	this.xSpeed += vector1.xSpeed;
+	this.ySpeed += vector1.ySpeed;
 }
 //subtrahera given inparameters vektor med denna vektorn
 public void subVector(posVector vector1){
-	this.xSpeed -= vector1.getX();
-	this.ySpeed -= vector1.getY();
+	this.xSpeed -= vector1.xSpeed;
+	this.ySpeed -= vector1.ySpeed;
 }
 //Förflytta denna vektorns position (xPosition, yPosition) med (xSpeed, ySpeed) antal pixlar
 public void moveVector(){
@@ -52,12 +50,18 @@ public posVector normalize(){
 	}
 	return returnvec;
 }
-public double distance(posVector vector1){
+public double squaredistance(posVector vector1){
+	double dX = this.xPosition - vector1.xPosition;
+	double dY = this.yPosition - vector1.yPosition;
+	return dX*dX+dY*dY;
 }
-public float getX(){
+public double distance(posVector vector1){
+	return Math.sqrt(squaredistance(vector1));
+}
+public double getX(){
 	return this.xSpeed;
 }
-public float getY(){
+public double getY(){
 	return this.ySpeed;
 }
 @Override
@@ -69,8 +73,4 @@ public posVector clone(){
 		throw new InternalError();
 	}
 }
-
-
-
-
 }
