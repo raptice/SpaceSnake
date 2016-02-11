b7d4b65de2278ebd14856085f2c3758b6838af28
Micaela Carlsson <micaela@dhcp-079103.eduroam.chalmers.se>
2016-02-11 10:00:45 +0100

    Added that controller prints which button is pressed

--- src/MenuController/MainController.java
+++ src/MenuController/MainController.java
@@ -16,6 +16,18 @@ public class MainController implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Controller ActionEvent");
+		if (e.getActionCommand() == "Button1") {
+			//different than the others because of different code in StartupMenu in the GUI package
+			System.out.println("Controller: Button1");
+		}
+		else if (e.getActionCommand() == "Command2") {
+			System.out.println("Controller: Button2");
+		}
+		else if (e.getActionCommand() == "Command3") {
+			System.out.println("Controller: Button3");
+		}
+		else {
+			System.out.println("Controller: Unknown button: " + e.paramString()); //debugging
+		}
 	}
 }
