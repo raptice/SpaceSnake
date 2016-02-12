de73399e9bbef961626c98eda16d23fc50c5e652
SamtherealMVP <victor.christoffersson@telia.com>
2016-02-12 11:52:07 +0100

    Files from ModelTeam
    Please jebus, work!

--- src/model/Floater.java
+++ src/model/Floater.java
@@ -1,11 +1,30 @@
 package model;
+import java.util.List;
+import util.*;
 
-public class Floater extends Vector2D implements Gravity{
-	private Vector2D position;
-	
-	public Floater(double xSpeed, double ySpeed, double xPos, double yPos){
-		super(xSpeed, ySpeed);
-		this.position.setX(xPos);
-		this.position.setY(yPos);
+public class Floater extends WorldObject implements Gravity{
+	private Vector2D velocity;
+	private double gravity;
+	public Floater(double xSpeed, double ySpeed, double xPos, double yPos, double mass){
+		super(xPos, yPos, mass);
+		this.velocity.setX(xPos);
+		this.velocity.setY(yPos);
 	}
+	@Override
+	public Vector2D getGravity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Vector2D calcuateGravity(Vector2D arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void gravityPull(List<WorldObject> argList) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
