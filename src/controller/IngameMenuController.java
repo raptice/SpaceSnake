391ec37597c2a7c796fd17d5938e843dd3dcecf0
Micaela Carlsson <micaela@Micaelas-MacBook-Air.local>
2016-02-13 16:14:22 +0100

    Fixed typo
    It was making the startup menu appear instead of the ingame menu

--- src/controller/IngameMenuController.java
+++ src/controller/IngameMenuController.java
@@ -1,5 +1,30 @@
+
 package controller;
 
-public class IngameMenuController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class IngameMenuController implements ActionListener {
+	private MainController parent;
+	
+	public IngameMenuController(MainController AL){
+		parent = AL;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand() == "Start new game") {
+			System.out.println("Controller: Button1");
+			//parent.startNewGame();
+		}
+		else if (e.getActionCommand() == "Load game") {
+			System.out.println("Controller: Button2");
+		}
+		else if (e.getActionCommand() == "Exit") {
+			System.out.println("Controller: Button3");
+			System.exit(0);
+		}
+		else {
+			System.out.println("Controller: Unknown button: " + e.paramString()); //debugging
+		}
+	}
 }
