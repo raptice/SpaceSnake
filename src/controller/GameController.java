6f5c7c9360173e3a967b04306bcfbec455ff9402
Micaela Carlsson <micaela@dhcp-075025.eduroam.chalmers.se>
2016-02-16 16:38:00 +0100

    Merged GameViewController with GameController

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import model.*;
+import view.*;
 
 import util.Command;
 
@@ -16,6 +17,7 @@ public class GameController implements ActionListener {
 	private WorldCollection worldCollection;
 	private WorldObject worldObject;
 	private PhysicsEngine physicsEngine;
+	private GameView gameView;
 	
 	private static final double testValue = 1;
 	private static final long longValue = 1;
@@ -26,12 +28,20 @@ public class GameController implements ActionListener {
 	 */
 	public GameController(MainController parent){
 		this.parent = parent;
-		
 		worldCollection = new WorldCollection();
+	}
+	
+	public void createWorld() {
 		physicsEngine = new PhysicsEngine(worldCollection, longValue);
 	}
 	
+	public void addView(GameView gameView) {
+		this.gameView = gameView;
+		//gameView.addWorld(worldCollection);
+	}
+	
 	//TODO: Create snake, create randomized object
+	//fill world with objects
 	public void createObject() {
 		worldObject = new Floater(testValue, testValue, testValue, testValue, testValue);
 		//for (worldCollection object: WorldObject)
@@ -50,6 +60,14 @@ public class GameController implements ActionListener {
 		if (e.getActionCommand() == Command.MOUSE_PRESSED) {
 			System.out.println("GameViewController: Mouse pressed");
 			//startAccelerating();
+		}
+		else if (e.getActionCommand() == Command.MOUSE_RELEASED) {
+			System.out.println("GameViewController: Mouse released");
+			//stopAccelerating();
+		}
+		else if (e.getActionCommand() == Command.MOUSE_DRAGGED) {
+			System.out.println("GameViewController: Mouse dragged");
+			//changeAccelerationDirection();
 		}
 		else {
 			System.out.println("GameViewController: Unknown button: " + e.paramString()); //debugging
