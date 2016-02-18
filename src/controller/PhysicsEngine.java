44a17ebf72dace2d5dc7b797b087f16ba49c71b6
Gustav Svensson <di0svgu@student.chalmers.se>
2016-02-18 10:33:23 +0100

    använder Moveable istället för tomt interface

--- src/controller/PhysicsEngine.java
+++ src/controller/PhysicsEngine.java
@@ -1,7 +1,7 @@
 package controller;
 
 import model.IGravity;
-import model.IMovable;
+import model.Moveable;
 import model.Physics;
 import model.WorldCollection;
 import model.WorldObject;
@@ -40,8 +40,8 @@ public class PhysicsEngine extends Thread
             	}       	
             }
             for(WorldObject obj : data.getCollection()){
-            	if(obj instanceof IMovable){
-            		((IMovable)obj).move();
+            	if(obj instanceof Moveable){
+            		((Moveable)obj).move();
             	}
             }
             for(WorldObject obj : data.getCollection()){
