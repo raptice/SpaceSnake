c88c81fb45c07ed27541cf9e402a419b01f62197
Gustav Svensson <di0svgu@student.chalmers.se>
2016-02-19 10:35:38 +0100

    buggfixar kollisionerna

--- src/model/Moveable.java
+++ src/model/Moveable.java
@@ -75,12 +75,8 @@ extends WorldObject
 	 */
 	public void Collision(WorldCollection data){
 		for(WorldObject obj : data.getCollection()){
-			if(!this.equals(obj)){
-				double length = Math.sqrt(this.getPosition().distancesquared(obj.getPosition()));
-				double radlength = this.getRadius() + obj.getRadius();
-				if(length < radlength && obj instanceof Moveable){
-					velocity.setValues(CollisionResponse(obj));
-				}
+			if(!this.equals(obj) && collides(obj)){
+					velocity_diff = velocity_diff.sub(CollisionResponse(obj));
 			}
 		}
 	}
@@ -100,21 +96,44 @@ extends WorldObject
 	 * @return Vector2D the collisionvector
 	 */
 	public Vector2D CollisionResponse(WorldObject obj){
-		Vector2D v1 = this.getVelocity();
-		Vector2D v2 = ((Moveable)obj).getVelocity();
-		Vector2D p1 = this.getPosition();
-		Vector2D p2 = ((Moveable)obj).getPosition();
-		double m1 = this.getMass();
-		double m2 = ((Moveable)obj).getMass();
-		double massScalar = (2*m2)/(m1+m2);
-		Vector2D p1minusp2 = p1.sub(p2);
-		Vector2D v1minusv2 = v1.sub(v2);
-		double dstsqr = p1minusp2.lengthsquared();
-		double dot = Vector2D.dot(v1minusv2, p1minusp2);
-		double constant = (massScalar * dot)/dstsqr;
-		Vector2D newV1 = v1.sub(p1minusp2.scale(constant));
-		newV1.setX(-newV1.getX());
-		newV1.setY(-newV1.getY());
-		return newV1; //the collision response: velocity_diff 
+		
+		
+		if (obj instanceof Moveable) 
+		{
+			
+			
+			Vector2D v1 = this.getVelocity();
+			Vector2D v2 = ((Moveable)obj).getVelocity();
+			Vector2D p1 = this.getPosition();
+			Vector2D p2 = ((Moveable)obj).getPosition();
+			double m1 = this.getMass();
+			double m2 = ((Moveable)obj).getMass();
+			double massScalar = (2*m2)/(m1+m2);
+			Vector2D p1minusp2 = p1.sub(p2);
+			Vector2D v1minusv2 = v1.sub(v2);
+			double dstsqr = p1minusp2.lengthsquared();
+			double dot = Vector2D.dot(v1minusv2, p1minusp2);
+			double constant = (massScalar * dot)/dstsqr;
+			Vector2D newV1 = p1minusp2.scale(constant);
+			return newV1; //the collision response: velocity_diff 
+			
+			/*
+			Moveable moveable = (Moveable) obj;
+			double dot = velocity.sub(moveable.getVelocity()).dot(position.sub(moveable.getPosition()));
+			Vector2D dv = position.sub(moveable.getPosition());
+			dv = dv.scale(  dot/position.sub(moveable.getPosition()).lengthsquared());
+			dv = dv.div(mass+moveable.getMass());
+			return dv.scale(collision_damping*2*moveable.getMass());
+			*/
+			
+		} else {
+			double dot = velocity.dot(position.sub(obj.getPosition()));
+			Vector2D dv = position.sub(obj.getPosition());
+			dv = dv.scale(  dot/position.sub(obj.getPosition()).lengthsquared());
+			return dv.scale(collision_damping*2);
+		}
+		
+		
+		
 	}
 }
