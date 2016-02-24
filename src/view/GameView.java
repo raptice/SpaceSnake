e2e181ba6886199694a26a9a7a33ab73df052257
Gustav Svensson <di0svgu@student.chalmers.se>
2016-02-24 19:06:47 +0100

    GameView visar edible

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -21,6 +21,7 @@ import model.Floater;
 import model.WorldCollection;
 import model.WorldObject;
 import model.objects.BlackHole;
+import model.objects.Edible;
 import model.objects.SnakeHead;
 import model.objects.SnakeTail;
 
@@ -214,6 +215,8 @@ implements MouseWheelListener, MouseMotionListener, MouseListener, GameObserver,
 		final GameFigure figure;
 		if (what instanceof Floater) {
 			figure = new FloaterView(what.getPosition().getX(), what.getPosition().getY(), what.getRadius()*2 ,this);
+		} else if (what instanceof Edible) {
+			figure = new EdibleView(what.getPosition().getX(), what.getPosition().getY(), what.getRadius()*2 ,this);
 		} else if (what instanceof BlackHole) {
 			figure = new BlackHoleView(what.getPosition().getX(), what.getPosition().getY(), what.getRadius()*2 ,this);
 		} else if (what instanceof SnakeHead) {
