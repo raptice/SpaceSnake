1c819ec182fb8b1da829d795dcfed5bbcc1d1012
micaelacarlsson <micaela@dhcp-074152.eduroam.chalmers.se>
2016-02-25 09:50:17 +0100

    Adds object creation class

--- src/controller/WorldFactory.java
+++ src/controller/WorldFactory.java
@@ -1,105 +1,34 @@
-
 package controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.Random;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-
-import model.*;
+import model.Floater;
+import model.WorldCollection;
+import model.WorldObject;
 import model.objects.BlackHole;
 import model.objects.Edible;
 import model.objects.SnakeHead;
 import model.objects.SnakeTail;
-import view.*;
-import util.Config;
-import util.GameEvent;
 import util.Vector2D;
 
-/**
- * Handles events from the GameView, creates a new world, creates objects and adds them to world...
- * TODO: Create new classes with more specified roles
- */
-public class GameController 
-implements ActionListener, Observer 
-{
-	private WorldCollection worldCollection;
-	private PhysicsEngine physicsEngine;
+public class WorldFactory {
 	
-	SnakeHead head;
-	MainController parent;
-
 	private static final int MAX_SPAWN = 50;
 	private static final int MIN_SPAWN = 20;
-
-	private static final long longValue = 50;
 	
-	/**
-	 * TODO: Do we need to create a new game here? 
-	 * It will always create one when you press "New Game" in the start menu, 
-	 * so it does this twice, thereby throwing away the first one?
-	 */
-	public GameController(MainController parent){
-		newGame();
-	}
+	SnakeHead head;
+	GameController parent;
+	WorldCollection worldCollection;
 	
-	/**
-	 * Starts the thread where the physics run
-	 */
-	public void newGame () {
-		worldCollection = new WorldCollection();
-		worldCollection.addObserver(this);
-		worldCollection.setWorldSize(Config.randomWorldSize());
-		physicsEngine = new PhysicsEngine(worldCollection, 1, longValue);
-		
-		head = null;
+	public WorldFactory(GameController parent, WorldCollection worldCollection) {
+		this.parent = parent;
+		this.worldCollection = worldCollection;
 		createObjects();
 	}
 	
-	/**
-	 * Starts the thread where the physics run
-	 */
-	public void runPhysics () {
-		physicsEngine.start();
-	}
-	
-	
-	/**
-	 * Pauses the thread where the physics run
-	 */
-	public void stopPhysics () {
-		physicsEngine.interrupt();
-	}
-	
-	public void pausePhysics() {
-		physicsEngine.setPaused();
-	}
-	
-	public void resumePhysics() {
-		synchronized(physicsEngine) {
-			physicsEngine.setResumed();
-			physicsEngine.notifyAll();
-		}
-	}
-	
-	
-	/*public void addView(GameView gameView) {
-		this.gameView = gameView;
-		this.mapView = mapView;
-		worldCollection.addObserver(gameView);
-		worldCollection.addObserver(mapView);
-		gameView.addWorld(worldCollection);
-	}*/
-	public void addObserver(GameObserver gameObserver) {
-		worldCollection.addObserver(gameObserver);
-		gameObserver.addWorld(worldCollection);
-	}
-
 	/**
 	 * Creates a snake and adds objects to the world
 	 * TODO: 	Probably move snake creation to a new separate method
@@ -135,7 +64,7 @@ implements ActionListener, Observer
 	public Map<String,Integer> randomSpawns() {		
 		Map<String,Integer> spawns = new HashMap<String,Integer>();
 		Random random = new Random();
-		int size = worldCollection.getWorldSize();
+		//int size = worldCollection.getWorldSize();
 		int totalObjects = random.nextInt(MAX_SPAWN)+ MIN_SPAWN;
 			
 			int floater = totalObjects/3;
@@ -218,46 +147,4 @@ implements ActionListener, Observer
 		else
 			return false;
 	}
-	
-	
-	/**
-	 * Handles events in the game
-	 * @param e		The ActionEvent sent from a mouse press
-	 */
-	public void actionPerformed(ActionEvent e_in) {
-		GameEvent e = (GameEvent) e_in;
-		if (e.getActionCommand() == GameEvent.MOUSE_PRESSED) {
-			//System.out.println("GameController: Mouse pressed: "+e.getVector());
-			this.physicsEngine.SnakePull(e.getVector().div(100));
-			//Maybe should set something in the physicsengine that released unsets?
-			//head.startAccelerating();
-		}
-		else if (e.getActionCommand() == GameEvent.MOUSE_RELEASED) {
-			//System.out.println("GameController: Mouse released: "+e.getVector());
-			//head.stopAccelerating();
-			this.physicsEngine.SnakePull(null);
-		}
-		else if (e.getActionCommand() == GameEvent.MOUSE_DRAGGED) {
-			//System.out.println("GameController: Mouse dragged: "+e.getVector());
-			this.physicsEngine.SnakePull(e.getVector().div(100));
-			//head.changeAccelerationDirection();
-		}
-		else {
-			System.out.println("GameViewController: Unknown button: " + e.paramString()); //debugging
-		}
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		if(arg1 instanceof String)
-		{	
-			if (((String)arg1).equals("GAMEOVER"))
-			{
-				System.out.println("GAME OVER");
-				parent.setGameOver();
-				pausePhysics(); //Bad!
-			}
-		}
-	}
-	
 }
